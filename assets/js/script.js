fa58bec362b17839e69676f484004df44b77655a
unknown <alicia9790972@gmail.com>
2022-09-23 21:25:13 -0400

    addling local storage and recall

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,9 +20,63 @@ init();
 
 //make time blocks here
 
+//save button
+$(".saveBtn").click(function () {
+  savedText = $(this).siblings(".txt").val();
+  console.log(eventText);
+  savedTime = $(this).siblings(".hour").text();
+  console.log(savedTime);
+  localStorage.setItem(savedTime, JSON.stringify(savedText));
+
+  inputText();
+  pastPresent();
+});
+
+//placing saved text
+function inputText() {
+  var saved1 = JSON.parse(localStorage.getItem("10:00 am"));
+  $("#1").val(saved1);
+
+  var saved2 = JSON.parse(localStorage.getItem("10:00 am"));
+  $("#2").val(saved2);
+
+  var saved3 = JSON.parse(localStorage.getItem("10:00 am"));
+  $("#3").val(saved3);
+
+  var saved4 = JSON.parse(localStorage.getItem("10:00 am"));
+  $("#4").val(saved4);
+
+  var saved5 = JSON.parse(localStorage.getItem("10:00 am"));
+  $("#5").val(saved5);
+
+  var saved6 = JSON.parse(localStorage.getItem("10:00 am"));
+  $("#6").val(saved6);
+
+  var saved7 = JSON.parse(localStorage.getItem("10:00 am"));
+  $("#7").val(saved7);
+
+  var saved8 = JSON.parse(localStorage.getItem("10:00 am"));
+  $("#8").val(saved8);
+
+  var saved9 = JSON.parse(localStorage.getItem("10:00 am"));
+  $("#9").val(saved9);
+
+  var saved10 = JSON.parse(localStorage.getItem("10:00 am"));
+  $("#10").val(saved10);
+
+  var saved11 = JSON.parse(localStorage.getItem("10:00 am"));
+  $("#11").val(saved11);
+
+  var saved12 = JSON.parse(localStorage.getItem("10:00 am"));
+  $("#12").val(saved12);
+
+  var saved13 = JSON.parse(localStorage.getItem("10:00 am"));
+  $("#13").val(saved13);
+}
+
 //display colour change
 function pastPresent() {
-  $(".input").each(function () {
+  $(".txt").each(function () {
     var scheduledTime = parseInt($(this).attr("id"));
     console.log(scheduledTime);
 
@@ -41,8 +95,4 @@ function pastPresent() {
     }
   });
 }
-
-//save button
-saveButton.addEventListener("click", blockClick);
-
-//when refreshed info stays
+//when refreshed info stay
